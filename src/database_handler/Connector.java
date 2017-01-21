21760cd23440f2d079521d87a6c7672af70dde27
Tomasz <elkapitano40@gmail.com>
2017-01-21 19:12:46 +0100

    TODO jdbc

--- src/database_handler/Connector.java
+++ src/database_handler/Connector.java
@@ -2,4 +2,11 @@ package database_handler;
 
 public class Connector {
 
+	//TODO jdbc driver init
+	//TODO generyczna metoda do wywolania sql
+//	public static Liste stringow executeSql(String sql, lista parametrow);
+//	laczenie z baza danych, 
+//	wsadzenie odpowiednich parametrow z listy parametrow, 
+//	zwrocic liste wynikow
+	
 }
