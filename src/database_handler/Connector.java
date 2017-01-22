93cbacc96770bd6ac9716630542853ebfb616cb2
ferot <feeroo@gmail.com>
2017-01-22 14:24:37 +0100

    Added executeQuery basic option + simple test for main

--- src/database_handler/Connector.java
+++ src/database_handler/Connector.java
@@ -7,18 +7,9 @@ public class Connector {
 static final String USER = "tferens";
 static final String PASS = "tferens";
 
-static final String JDBC_DRIVER = "df";
 static final String DB_URL = "jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf";
 
 static Connection conn = null;
-static {
-	try {
-		Class.forName("oracle.jdbc.OracleDriver");
-	} catch (ClassNotFoundException e) {
-		System.err.println("Driver not found.");
-		System.exit(1);
-	}
-}
 
 public void connect() {
 	
@@ -27,11 +18,12 @@ public void connect() {
 	try{
 		
 	System.out.println("Connecting to database...");
+	
 	Class.forName("oracle.jdbc.OracleDriver");
-	conn = DriverManager.getConnection(DB_URL,USER,PASS);
+	conn = DriverManager.getConnection(DB_URL, USER, PASS);
 	
 	} catch (ClassNotFoundException e) {
-		System.err.println("SQL driver not found.");
+		System.err.println("SQL driver not found! Exiting...");
 		System.exit(1);	
 	}catch(SQLException sqlEx){
 		sqlEx.printStackTrace();
@@ -52,5 +44,20 @@ public void disconnect(){
 			sqlEx.printStackTrace();
 		}
 		System.out.println("Successfully closed connection to database ...");
+}
+
+public ResultSet executeQuery(String query) {
+	System.out.println("Creating Statement ...");
+	Statement stmt = null;
+	ResultSet rs = null;
+	try{
+	stmt = conn.createStatement();
+	rs = stmt.executeQuery(query);
+	return rs;
+	}catch (SQLException sqlEx){
+		System.out.println("Couldn't prepare statement");
+		sqlEx.printStackTrace();
+	}
+	return rs;
 }
 }
